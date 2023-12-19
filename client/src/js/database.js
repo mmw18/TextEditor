a6da38105f4d4119a4f31fa232c6b2d373fd5c88
Megan Wright <meganmwright2018@gmail.com>
2023-12-18 22:43:42 -0600

    Adding put method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+export const putDb = async (content) => {
+  try {
+    // Opening database, named textEditor_db, version 1
+    const db = await openDB('textEditor_db', 1);
+    // Starting a new read/write transaction on the 'notes' object store
+    const tx = db.transaction('notes', 'readwrite');
+    // Accessing the 'notes' object store
+    const store = tx.objectStore('notes');
+    // Attempt to add or update the content within the store
+    const request = store.put(content);
+    // Waiting for the request to complete and get the result
+    const result = await request;
+    // Ensuring transaction is completed before moving on
+    await tx.done;
+    // Logging successful operation to the console
+    console.log('Data saved to the database', result);
+    // Returning result - for processing or confirmation
+    return result; 
+    // Logging any errors that occur during operation
+  } catch (error) {
+    console.error('Error putting data into IndexedDB', error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
