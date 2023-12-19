667e30d9ab9d75e71308886c3c96814bc8f0eb32
Megan Wright <meganmwright2018@gmail.com>
2023-12-19 15:45:41 -0600

    asset and image caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,34 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Asset caching
+registerRoute(
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // Caching responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+// Image caching
+registerRoute(
+  ({ request }) => request.destination === 'image',
+
+  new StaleWhileRevalidate({
+    // Name of cache storage - for images
+    cacheName: 'image-cache', 
+    plugins: [
+      new CacheableResponsePlugin({
+        // Cache responses with status 0 (opaque) and 200 (success).
+        statuses: [0, 200], 
+      }),
+    ],
+  })
+);
